e2697703188b3d8b1a43b4bb424dbc5089a09093
GitHub Action <action@github.com>
2021-10-20 06:35:01 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/axnshy/gradleplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.axnshy.gradleplugintemplate.services
+
+import com.github.axnshy.gradleplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/axnshy/gradleplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/axnshy/gradleplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.axnshy.gradleplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.axnshy.gradleplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
